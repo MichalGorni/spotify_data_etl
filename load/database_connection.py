8aed7995b976cda3b35d320705555414099b9dfa
Michal Gorniewski <gorniewskimichall@gmail.com>
2023-09-30 22:01:09 +0200

    Added module providing connection to the database.

---
+++ load/database_connection.py
@@ -0,0 +1,28 @@
+import sqlite3
+import os
+import pandas as pd
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+class DataBaseConnector:
+    """
+    Provides connection to sqlite database.
+    """
+
+    def __init__(self) -> None:
+        self.connection = sqlite3.connect(rf"{BASE_DIR}\database.db")
+        self.cursor = self.connection.cursor()
+
+    def load_table_to_database(self, df: pd.DataFrame, table_name: str) -> None:
+        """
+        Loads data from given DataFrame into give table in sqlite database.
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            DataFrame object containing data to be loaded into database.
+        table_name: str
+            Name of table to load data.
+        """
+        df.to_sql(table_name, self.connection, index=False, if_exists="replace")
